5de048d89af4e06d7a5450e68c5492a8f8f5bfe7
Aleksander <asava@sibmail.com>
2022-07-20 16:30:54 +0700

    пол таблицы есть

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,81 +1,76 @@
 //  Не смог доконца
 
 
+import React, {useState} from "react";
+import { Container, Row, Col, Table } from "react-bootstrap";
+import api from '../api';
+// let allUsers = api.users.fetchAll();
 
 
-import { Component } from 'react'
-import { Table, Container, Row, Col } from "react-bootstrap"
-import api from '../api'
-
-let allUsers = api.users.fetchAll()
-
-export default class App extends Component {
-    state = {
-    rows: allUsers
+let UserTable = () => {
     
-    
-    }
+    const [table, setTable] = useState (api.users.fetchAll());
 
-    spliceRow = (index) => {
-    this.state.rows.splice(index, 1)
-    this.setState({ rows: this.state.rows })
-    }
+    let handleDelete = (id) => {
+        console.log('handle delete', id);
+        let newListUsers = table.filter(u => u._id !== id);
+        console.log('newListUsers', newListUsers);
+        setTable(newListUsers)
+    };
+
+    let message = table.length === 2 || 
+                      table.length === 3 || 
+                      table.length === 4
+                      ? 'человека тусанут' 
+                      : 'человек тусанет'
 
 
-
-    render() {
-        let message = allUsers.length === 2 || 
-                      allUsers.length === 3 || 
-                      allUsers.length === 4
-                      ? 'человека' 
-                      : 'человек'
     return (
         <>
-            <Container className="mt-3">
+        <Container className="mt-3">
                 <Row>
                     <Col>
-                        <h3 className='text-primary'> {allUsers.length} {message} тусанет с тобой сегодня! </h3>
-                    </Col>
-                    </Row>
-                <Row>
-                    <Col>
-                        <Table striped bordered hover className="shadow-lg">
-                            <thead>
-                                <tr>
-                                    <th>Имя</th>
-                                    <th>Качества</th>
-                                    <th>Провфессия</th>
-                                    <th>Встретился, раз</th>
-                                    <th>Оценка</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {this.state.rows.map((row, index) => {
-                                return (
-                                <tr key={row._id}>
-                                        <td>{row.name}</td>
-                                        <td>{row.qualities._name}</td>
-                                        <td>{row.profession.name}</td>
-                                        <td>{row.completedMeetings}</td>
-                                        <td>{row.rate}</td>
-
-                                        <td>
-                                        <button 
-                                            className="btn btn-danger btn-sm m-2" 
-                                            onClick={() => this.spliceRow(index)}>
-                                            DEL
-                                        </button>
-                                        </td>
-                                </tr>
-                                )
-                            })}
-                            </tbody>
-                        </Table>
+                        <h3 className='text-primary'> {table.length} {message} с тобой сегодня </h3>
                     </Col>
                 </Row>
-            </Container>
+            <Table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Имя</th>
+                        <th scope="col">Качества</th>
+                        <th scope="col">Првфесия</th>
+                        <th scope="col">Встреч</th>
+                        <th scope="col">Оценка</th>
+                        <th scope="col">Кнопка</th>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    {
+                    table.map(user => {
+                        return (
+                            <tr key={user._id}>
+                                <td>{user.name}</td>
+                                <td>{user.qualities._name}</td>
+                                <td>{user.profession.name}</td>
+                                <td>{user.completedMeetings}</td>
+                                <td>{user.rate}</td>
+                                <td><button 
+                                    className="btn btn-danger btn-sm m-2" 
+                                    onClick={() => handleDelete(user._id)}
+                                    >
+                                        Delete
+                                    </button></td> 
+                            </tr>
+                        )
+                    })
+                }
+                </tbody>
+            </Table>
+        </Container>
         </>
     )
-    }
-}   
+};
+
+
+export default UserTable;
