91bd66d5e0845b6a542d368d32a4d916e9b92318
Aleksander <asava@sibmail.com>
2022-07-21 14:20:10 +0700

    таблица удаление работает

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,6 +1,3 @@
-//  Не смог доконца
-
-
 import React, {useState} from "react";
 import { Container, Row, Table } from "react-bootstrap";
 import api from '../api';
@@ -27,10 +24,38 @@ let UserTable = () => {
             ? 'человека тусанут' 
             : 'человек тусанет';
 
+    // const message = () => {
+    //     return table.length === 2 || 
+    //     table.length === 3 || 
+    //     table.length === 4 
+    //     ? 'человека тусанут' 
+    //     : 'человек тусанет';
+    // };
+
     let header = 
             table.length === 0 
             ? <span className="badge bg-danger">Никто не тусанет с тобой</span> 
-            : <span className="badge bg-primary">{table.length} {message} с тобой сегодня</span>;
+            : <span className="badge bg-primary">{table.length} {message} с тобой сегодн</span>;
+    
+    // const header = () => {
+    //     return table.length === 0 
+    //     ? <span className="badge bg-danger">Никто не тусанет с тобой</span> 
+    //     : <span className="badge bg-primary">{table.length} {messag()} с тобой сегодня</span>;
+    // };
+
+    const renderTableHeader = () => {
+        if(table.length===0) return ""
+        return (
+            <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Првфесия</th>
+                <th scope="col">Встреч</th>
+                <th scope="col">Оценка</th>
+                <th scope="col">Кнопка</th>
+            </tr>
+        )
+    };
 
     return (
         <>
@@ -42,14 +67,7 @@ let UserTable = () => {
                 </Row>
             <Table className="table">
                 <thead>
-                    <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
-                        <th scope="col">Првфесия</th>
-                        <th scope="col">Встреч</th>
-                        <th scope="col">Оценка</th>
-                        <th scope="col">Кнопка</th>
-                    </tr>
+                    {renderTableHeader()}
 
                 </thead>
                 <tbody>
@@ -62,7 +80,7 @@ let UserTable = () => {
                                 <td>{user.name}</td>
 
                                 <td>{user.qualities.map(job => {
-                                    return <span className="badge bg-primary m-1" key={job._id}>{job.name}</span>
+                                    return <span className={`badge bg-${job.color} m-1`} key={job._id}>{job.name}</span>;
                                     
                                 })}</td>
 
