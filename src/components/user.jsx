516da510308c6dba0251d6ea169dbcb766101d7a
Aleksander <asava@sibmail.com>
2022-07-22 11:30:44 +0700

    Добавил решение от Владилена

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,108 +1,74 @@
-import React, {useState} from "react";
-import { Container, Row, Table } from "react-bootstrap";
-import api from '../api';
-// let allUsers = api.users.fetchAll()
+import React, { useState } from "react";
+import api from "../api"
 
+const Users = () => {
+    const [users, setUsers] = useState(api.users.fetchAll());
 
-
-
-let UserTable = () => {
-    
-    const [table, setTable] = useState (api.users.fetchAll());
-
-    let handleDelete = (id) => {
-        console.log('handle delete', id);
-        let newListUsers = table.filter(u => u._id !== id);
-        console.log('newListUsers', newListUsers);
-        setTable(newListUsers)
+    const handleDelete = (userId) => {
+        setUsers(users.filter((user) => user._id !== userId));
     };
 
-    let message = 
-            table.length === 2 || 
-            table.length === 3 || 
-            table.length === 4 
-            ? 'человека тусанут' 
-            : 'человек тусанет';
-
-    // const message = () => {
-    //     return table.length === 2 || 
-    //     table.length === 3 || 
-    //     table.length === 4 
-    //     ? 'человека тусанут' 
-    //     : 'человек тусанет';
-    // };
-
-    let header = 
-            table.length === 0 
-            ? <span className="badge bg-danger">Никто не тусанет с тобой</span> 
-            : <span className="badge bg-primary">{table.length} {message} с тобой сегодня</span>;
-    
-    // const header = () => {
-    //     return table.length === 0 
-    //     ? <span className="badge bg-danger">Никто не тусанет с тобой</span> 
-    //     : <span className="badge bg-primary">{table.length} {messag()} с тобой сегодня</span>;
-    // };
-
-    const renderTableHeader = () => {
-        if(table.length===0) return ""
-        return (
-            <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Првфесия</th>
-                <th scope="col">Встреч</th>
-                <th scope="col">Оценка</th>
-                <th scope="col">Кнопка</th>
-            </tr>
-        )
+    const renderPhrase = (number) => {
+        const lastOne = Number(number.toString().slice(-1));
+        if (number > 4 && number < 15) return "человек тусанет";
+        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+        if (lastOne === 1) return "человек тусанет";
+        return "человек тусанет";
     };
 
     return (
         <>
-        <Container className="mt-3">
-                <Row>
-                    <div>
-                        <h1 className='text-primary'>{header}</h1>;
-                    </div>
-                </Row>
-            <Table className="table">
-                <thead>
-                    {renderTableHeader()}
+            <h2>
+                <span
+                    className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+                >
+                    {users.length > 0
+                        ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
+                        : "Никто с тобой не тусанет"}
+                </span>
+            </h2>
 
-                </thead>
-                <tbody>
-                    {
-            
-                    table.map(user => {
-                       
-                        return (
+            {users.length > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
                             <tr key={user._id}>
-                                <td>{user.name}</td>
-
-                                <td>{user.qualities.map(job => {
-                                    return <span className={`badge bg-${job.color} m-1`} key={job._id}>{job.name}</span>;
-                                    
-                                })}</td>
-
-                                <td>{user.profession.name}</td>
-                                <td>{user.completedMeetings}</td>
-                                <td>{user.rate} /5</td>
-                                <td><button 
-                                    className="btn btn-danger btn-sm m-2" 
+                            <td>{user.name}</td>
+                            <td>
+                                {user.qualities.map((item) => (
+                                    <span className={"badge m-1 bg-" + item.color} key={item._id}>
+                                        {item.name}
+                                    </span>
+                                ))}
+                            </td>
+                            <td>{user.profession.name}</td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{user.rate} /5</td>
+                            <td>
+                                <button
                                     onClick={() => handleDelete(user._id)}
-                                    >
-                                        Delete
-                                    </button></td> 
-                            </tr>
-                        )
-                    })
-                }
-                </tbody>
-            </Table>
-        </Container>
+                                    className="btn btn-danger"
+                                >
+                                    delete
+                                </button>
+                            </td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </>
-    )
+    );
 };
 
-
-export default UserTable;
+export default Users;
