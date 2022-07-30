d90e5c873317749fa58bf59be6209f906076062f
Aleksander <asava@sibmail.com>
2022-07-30 23:44:55 +0700

    end na 30-07-22

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,13 +1,24 @@
 
-import React from "react";
+import React, { useState } from "react";
+import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import User from "./user";
 const Users = ({ users, ...rest }) => {
     const count = users.length;
     const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1)
     const handlePageChange = (pageIndex) => {
-        console.log('page:', pageIndex)
-    }
+        console.log('page:', pageIndex);
+        setCurrentPage(pageIndex);
+    };
+   
+
+    const userCrop = paginate(users, currentPage, pageSize);
+
+    
+    
+    
+    
     return (
         <>
             {count > 0 && (
@@ -24,13 +35,17 @@ const Users = ({ users, ...rest }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map((user) => (
+                        {userCrop.map((user) => (
                             <User key={user._id} {...rest} {...user} />
                         ))}
                     </tbody>
                 </table>
             )}
-            <Pagination itemsCount={count} pageSize={pageSize} onPageChange={handlePageChange}/>
+            <Pagination 
+                itemsCount={count} 
+                pageSize={pageSize} 
+                currentPage ={currentPage}
+                onPageChange={handlePageChange}/>
         </>
     );
 };
